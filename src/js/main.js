c7b5d54fd6e906215fbf94b4100f214fdb77be71
vany pupkin <vany_pupkin@mail.ru>
2021-02-23 13:46:40 +0600

    add starting template

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//открытие закрытие выпадающего меню
+@@include('modules/_main-menu.js');
+
+//открытие закрытие выпадающего меню
+@@include('modules/_hamburger.js');
+
+//переключение слайдов
+@@include('modules/_slider.js');
+
+//переключение акордиона
+@@include('modules/_accordion.js');
+
+//поддержка формата webp в css
+@@include('vendor/_modernizr-webp.js');
+
+
