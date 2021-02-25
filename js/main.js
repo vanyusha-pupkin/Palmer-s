237c18717f8bc4f078163ef8257658dd7bd9eb7b
vany pupkin <vany_pupkin@mail.ru>
2021-02-25 15:20:03 +0600

    Update 2021-02-25T09:19:36.594Z

--- js/main.js
+++ js/main.js
@@ -15,9 +15,63 @@ hamburger.addEventListener('click', function() {
 ;
 
 //переключение слайдов
+var sliderControl_1 = document.querySelector('.controls__toggle--1');
+var sliderControl_2 = document.querySelector('.controls__toggle--2');
+var sliderControl_3 = document.querySelector('.controls__toggle--3');
+
+var sliderItem_1 = document.querySelector('.slider-main__item--1');
+var sliderItem_2 = document.querySelector('.slider-main__item--2');
+var sliderItem_3 = document.querySelector('.slider-main__item--3');
+
+
+sliderControl_1.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.add('is-active');
+  sliderItem_2.classList.remove('is-active');
+  sliderItem_3.classList.remove('is-active');
+
+  sliderControl_1.classList.add('is-active');
+  sliderControl_2.classList.remove('is-active');
+  sliderControl_3.classList.remove('is-active');
+})
+
+sliderControl_2.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.remove('is-active');
+  sliderItem_2.classList.add('is-active');
+  sliderItem_3.classList.remove('is-active');
+
+  sliderControl_1.classList.remove('is-active');
+  sliderControl_2.classList.add('is-active');
+  sliderControl_3.classList.remove('is-active');
+})
+
+sliderControl_3.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.remove('is-active');
+  sliderItem_2.classList.remove('is-active');
+  sliderItem_3.classList.add('is-active');
+
+  sliderControl_1.classList.remove('is-active');
+  sliderControl_2.classList.remove('is-active');
+  sliderControl_3.classList.add('is-active');
+})
 ;
 
 //переключение акордиона
+;
+
+//img-to-bg
+function imgToBG(){
+  let imgToBG=document.querySelectorAll(".img-to-bg");
+  for (var i = 0; i < imgToBG.length; i++) {
+    if(imgToBG[i].querySelector('img')){
+      imgToBG[i].style.backgroundImage = 'url('+imgToBG[i].querySelector('img').getAttribute('src')+')';
+    }
+  }
+}
+
+imgToBG();
 ;
 
 //поддержка формата webp в css
@@ -609,5 +663,3 @@ Tests for all forms of webp support (lossless, lossy, alpha, and animated)..
 ;
 
 })(window, document);;
-
-
