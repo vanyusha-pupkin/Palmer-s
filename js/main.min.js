3205c3b869c439c7d3bc26cf2330bf29a33c4ee6
vany pupkin <vany_pupkin@mail.ru>
2021-02-26 11:41:58 +0600

    Update 2021-02-26T05:41:23.820Z

--- js/main.min.js
+++ js/main.min.js
@@ -1,79 +1,5 @@
 'use strict';
 
-//открытие закрытие выпадающего меню
-;
-
-//открытие закрытие выпадающего меню
-
-var hamburger = document.querySelector('.page-header__burger');
-var mainNav = document.querySelector('.main-nav__list');
-
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('active');
-  mainNav.classList.toggle('open')
-})
-;
-
-//переключение слайдов
-var sliderControl_1 = document.querySelector('.controls__toggle--1');
-var sliderControl_2 = document.querySelector('.controls__toggle--2');
-var sliderControl_3 = document.querySelector('.controls__toggle--3');
-
-var sliderItem_1 = document.querySelector('.slider-main__item--1');
-var sliderItem_2 = document.querySelector('.slider-main__item--2');
-var sliderItem_3 = document.querySelector('.slider-main__item--3');
-
-
-sliderControl_1.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  sliderItem_1.classList.add('is-active');
-  sliderItem_2.classList.remove('is-active');
-  sliderItem_3.classList.remove('is-active');
-
-  sliderControl_1.classList.add('is-active');
-  sliderControl_2.classList.remove('is-active');
-  sliderControl_3.classList.remove('is-active');
-})
-
-sliderControl_2.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  sliderItem_1.classList.remove('is-active');
-  sliderItem_2.classList.add('is-active');
-  sliderItem_3.classList.remove('is-active');
-
-  sliderControl_1.classList.remove('is-active');
-  sliderControl_2.classList.add('is-active');
-  sliderControl_3.classList.remove('is-active');
-})
-
-sliderControl_3.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  sliderItem_1.classList.remove('is-active');
-  sliderItem_2.classList.remove('is-active');
-  sliderItem_3.classList.add('is-active');
-
-  sliderControl_1.classList.remove('is-active');
-  sliderControl_2.classList.remove('is-active');
-  sliderControl_3.classList.add('is-active');
-})
-;
-
-//переключение акордиона
-;
-
-//img-to-bg
-function imgToBG(){
-  let imgToBG=document.querySelectorAll(".img-to-bg");
-  for (var i = 0; i < imgToBG.length; i++) {
-    if(imgToBG[i].querySelector('img')){
-      imgToBG[i].style.backgroundImage = 'url('+imgToBG[i].querySelector('img').getAttribute('src')+')';
-    }
-  }
-}
-
-imgToBG();
-;
-
 //поддержка формата webp в css
 /*!
  * modernizr v3.6.0
@@ -663,3 +589,126 @@ Tests for all forms of webp support (lossless, lossy, alpha, and animated)..
 ;
 
 })(window, document);;
+
+//открытие закрытие выпадающего меню
+;
+
+//открытие закрытие выпадающего меню
+
+var hamburger = document.querySelector('.page-header__burger');
+var mainNav = document.querySelector('.main-nav__list');
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('active');
+  mainNav.classList.toggle('open')
+})
+;
+
+//переключение слайдов
+var sliderControl_1 = document.querySelector('.controls__toggle--1');
+var sliderControl_2 = document.querySelector('.controls__toggle--2');
+var sliderControl_3 = document.querySelector('.controls__toggle--3');
+
+var sliderItem_1 = document.querySelector('.slider-main__item--1');
+var sliderItem_2 = document.querySelector('.slider-main__item--2');
+var sliderItem_3 = document.querySelector('.slider-main__item--3');
+
+
+sliderControl_1.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.add('is-active');
+  sliderItem_2.classList.remove('is-active');
+  sliderItem_3.classList.remove('is-active');
+
+  sliderControl_1.classList.add('is-active');
+  sliderControl_2.classList.remove('is-active');
+  sliderControl_3.classList.remove('is-active');
+})
+
+sliderControl_2.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.remove('is-active');
+  sliderItem_2.classList.add('is-active');
+  sliderItem_3.classList.remove('is-active');
+
+  sliderControl_1.classList.remove('is-active');
+  sliderControl_2.classList.add('is-active');
+  sliderControl_3.classList.remove('is-active');
+})
+
+sliderControl_3.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderItem_1.classList.remove('is-active');
+  sliderItem_2.classList.remove('is-active');
+  sliderItem_3.classList.add('is-active');
+
+  sliderControl_1.classList.remove('is-active');
+  sliderControl_2.classList.remove('is-active');
+  sliderControl_3.classList.add('is-active');
+})
+;
+
+//переключение акордиона
+;
+
+//img-to-bg
+
+// как должен быть написан блок
+// <div class="img-to-bg">
+//   <picture>
+//     <source type="image/webp" srcset="img/image.webp">
+//     <img src="img/image.jpg" alt="">
+//   </picture>
+// </div>
+
+// оригинальная функция
+// function imgToBG(){
+//   let imgToBG=document.querySelectorAll(".img-to-bg");
+//   for (var i = 0; i < imgToBG.length; i++) {
+//     if(imgToBG[i].querySelector('img')){
+//       imgToBG[i].style.backgroundImage = 'url('+imgToBG[i].querySelector('img').getAttribute('src')+')';
+//     }
+//   }
+// }
+
+// imgToBG();
+
+
+// для webp
+function imgToBbWebp(){
+  let imgToBG=document.querySelectorAll(".img-to-bg");
+  for (var i = 0; i < imgToBG.length; i++) {
+
+    if(imgToBG[i].querySelector('source')){
+      imgToBG[i].style.backgroundImage = 'url('+imgToBG[i].querySelector('source').getAttribute('srcset')+')';
+    }
+
+  }
+}
+
+// для остальных типов изображений
+function imgToBb(){
+  let imgToBG=document.querySelectorAll(".img-to-bg");
+  for (var i = 0; i < imgToBG.length; i++) {
+
+    if(imgToBG[i].querySelector('img')){
+      imgToBG[i].style.backgroundImage = 'url('+imgToBG[i].querySelector('img').getAttribute('src')+')';
+    }
+
+  }
+}
+
+
+Modernizr.on('webp', function (result) {
+  if (result) {
+    // если браузер поддерживает webp
+    imgToBbWebp()
+  }
+  else {
+    // если браузер не поддерживает webp
+    imgToBb()
+  }
+});
+;
+
+
